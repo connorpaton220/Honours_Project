cf304e9f4cda0490a72c742aa6d2b976d6c4a484
Connor Paton <cpaton202@gmail.com>
2026-05-26 16:22:08 +0200

    token

--- run_select.sh
+++ run_select.sh
@@ -4,5 +4,6 @@
 #SBATCH --nodes=1 --ntasks=1
 #SBATCH --time=10:00
 #SBATCH --job-name="SelectSources"
+export HF_TOKEN=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
 module load python/miniconda3-py3.12
 python select_sources.py
