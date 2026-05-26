85108dea54c23f59776f13f3738285c62c25f2b6
Connor Paton <cpaton202@gmail.com>
2026-05-26 16:24:35 +0200

    removed token

--- run_select.sh
+++ run_select.sh
@@ -4,6 +4,6 @@
 #SBATCH --nodes=1 --ntasks=1
 #SBATCH --time=10:00
 #SBATCH --job-name="SelectSources"
-export HF_TOKEN=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
+export HF_TOKEN=
 module load python/miniconda3-py3.12
 python select_sources.py
